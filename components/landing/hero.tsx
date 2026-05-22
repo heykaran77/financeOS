d714a5a462025a891d7022fb0fe85ef0dd83cb94
Karan Singh <devkaran.codes@gmail.com>
2026-05-22 22:23:57 +0530

    chore: modify hero section

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -6,14 +6,14 @@ export default function Hero() {
     <div className="mx-auto max-w-5xl">
       <CustomGradientBlinds />
       <div className="pointer-events-none relative flex flex-col items-center justify-center py-20 text-center">
-        <div className="mb-4 flex items-center justify-center gap-4">
-          <Logo className="size-12 text-emerald-400 mix-blend-multiply" />
-          <h1 className="font-advercase-regular text-4xl tracking-tight text-white drop-shadow-lg md:text-6xl">
+        <div className="flex items-center justify-center gap-4">
+          <Logo className="size-10 text-neutral-200" />
+          <h1 className="font-advercase-regular text-4xl tracking-tight text-neutral-200 drop-shadow-lg md:text-5xl">
             FinanceOS
           </h1>
         </div>
-        <p className="text-md mt-4 max-w-2xl text-neutral-200">
-          Manage your money with clarity and confidence.
+        <p className="mt-4 max-w-2xl text-2xl font-medium tracking-tight text-neutral-200">
+          your no bullsh*t finance tracker
         </p>
       </div>
     </div>
