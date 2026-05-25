8bdbb8b3862e4bca62266e5278e073f80458c5d3
Karan Singh <devkaran.codes@gmail.com>
2026-05-25 21:53:37 +0530

    force light mode on landing page

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,10 +1,15 @@
 import Navbar from '@/components/common/navbar';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider
+      forcedTheme="light"
+      attribute="class"
+      disableTransitionOnChange
+    >
       <Navbar />
       {children}
-    </>
+    </ThemeProvider>
   );
 }
