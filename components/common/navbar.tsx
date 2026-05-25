0c9f4f142c23f7d5675fd0959a3a2e1fe3a05545
Karan Singh <devkaran.codes@gmail.com>
2026-05-25 14:25:35 +0530

    update: nav & hero sections

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -14,21 +14,25 @@ export default async function Navbar() {
   const user = session?.user;
   return (
     <nav className="pointer-events-none fixed top-4 left-1/2 z-50 flex w-full max-w-2xl -translate-x-1/2 items-center justify-between rounded-lg bg-neutral-800/20 px-4 py-4 backdrop-blur-sm">
-      <Link href={'/'} className="flex items-center gap-2">
+      <Link href={'/'} className="pointer-events-auto flex items-center gap-2">
         <Logo className="size-6 text-emerald-400" />
         <p className="text-md font-advercase-regular tracking-tight text-neutral-200">
           FinanceOS
         </p>
       </Link>
+
       <NavLinks
         navItems={NavbarConfig.navbarLinks}
-        classname="pointer-events-auto"
+        classname="pointer-events-auto hidden md:block"
       />
       {!user && (
-        <Button className="pointer-events-auto">
-          <Link href={'/auth/sign-up'} className="text-neutral-200">
+        <Button
+          className="pointer-events-auto"
+          render={<Link href={'/auth/sign-up'} />}
+        >
+          <span className="md:text-normal text-sm text-neutral-200">
             Get Started
-          </Link>
+          </span>
         </Button>
       )}
       {user && <Logout className="pointer-events-auto text-neutral-200" />}
--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -6,13 +6,13 @@ export default function Hero() {
     <div className="mx-auto max-w-5xl">
       <CustomGradientBlinds />
       <div className="pointer-events-none relative flex flex-col items-center justify-center py-20 text-center">
-        <div className="flex items-center justify-center gap-4">
-          <Logo className="size-10 text-neutral-200" />
-          <h1 className="font-advercase-regular text-4xl tracking-tight text-neutral-200 drop-shadow-lg md:text-5xl">
+        <div className="flex items-center justify-center gap-2 md:gap-4">
+          <Logo className="size-8 text-neutral-200 md:size-10" />
+          <h1 className="font-advercase-regular text-3xl tracking-tight text-neutral-200 drop-shadow-lg md:text-5xl">
             FinanceOS
           </h1>
         </div>
-        <p className="mt-4 max-w-2xl text-2xl font-medium tracking-tight text-neutral-200">
+        <p className="mt-4 max-w-2xl text-xl font-medium tracking-tight text-neutral-200 md:text-2xl">
           your no bullsh*t finance tracker
         </p>
       </div>
